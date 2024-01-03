2888e531a7aaad7f0422b9778de05bca902a1e3a
princeps911 <pakpobasa@gmail.com>
2024-01-03 15:26:51 -0800

    new 0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,34 +1,6 @@
-#include "lists.h"
-
-/**
- * insert_node - Inserts a number into a sorted singly-linked list.
- * @head: A pointer the head of the linked list.
- * @number: The number to insert.
- *
- * Return: If the function fails - NULL.
- * Otherwise - a pointer to the new node.
- */
-listint_t *insert_node(listint_t **head, int number)
-{
-        listint_t *node = *head, *new;
-
-        new = malloc(sizeof(listint_t));
-        if (new == NULL)
-                return (NULL);
-        new->n = number;
-
-        if (node == NULL || node->n >= number)
-        {
-                new->next = node;
-                *head = new;
-                return (new);
-        }
-
-        while (node && node->next && node->next->n < number)
-                node = node->next;
-
-        new->next = node->next;
-        node->next = new;
-
-        return (new);
-}
+#!/usr/bin/python3
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i * 10 + j < 89:
+            print("{:d}{:d}".format(i, j), end=", ")
+print("{:d}".format(89))
